cad604ee32b06e85d2733117d87deabaa5f66f10
lotpotcomedy189-jpg <lotpotcomedy189@gmail.com>
2026-07-07 10:13:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,17 +3,12 @@ import os
 ##Code Written By @ItsMeMaster
 
 class Config(object):
-    BOT_TOKEN = "8434843795:AAFamCgCGaQXXvTDkJ4FWpHnu_Dv4MmKxxE"
-    DB_NAME = "Yashxug"
-    API_ID = "22849789"
-    API_HASH = "0fc127c6055acd59f00ec6c229e1e3c4"
-    ADMIN_ID = 7296271316
-    DB_URL = "mongodb+srv://yashxug:Yash2835P@yashxug.om3yk1q.mongodb.net/retryWrites=true&w=majority&appName=Yashxug"
-    LOG_CHANNEL = "-1002477114210" # Your Log Channel ID (Bot ko ADMIN BNAYE)
-    USERLINK = ""
-    TUTORIAL_VIDEO = ""
-
-
-
-
-
+    BOT_TOKEN = "8641304240:AAFGFuvucyRRitrHmgQBpxuPWIFdsZFltvI"
+    DB_NAME = "nattu"
+    API_ID = "34422904"
+    API_HASH = "7e0002469784f47fc08a6b3d93d7ebed"
+    ADMIN_ID = "5349573682)"
+    DB_URL = "mongodb+srv://adarshppandey937:uIoPcln9vXQBF0vP@cluster0.o9mn6hb.mongodb.net/?"
+    LOG_CHANNEL = "-1003422914923" # Your Log Channel ID (Bot ko ADMIN BNAYE)
+    USERLINK = "https://t.me/Natking_uploader_bot"
+    TUTORIAL_VIDEO = "https://t.me/Natking_uploader_bot"
